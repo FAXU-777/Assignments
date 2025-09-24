77334ec8544244d39a8ccd7f67fe9506f8c1f26c
Nika <nikafaxuridze@gmail.com>
2025-09-24 03:14:26 -0700

    add new

--- HomeWork_7/Program.cs
+++ HomeWork_7/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("hello");
+        // 1. მოცემულია კვადრატი და კვადრატში ჩახატული წრე . ამ წრეში
+        // ჩახატულია კიდევ ერთი სხვა კვადრატი .
+        //     დაწერეთ პროგრამა რომელიც იპოვის სხვაობას დიდი და პატარა კვადრატის
+        //     ფართობებს შორის .
+        //     Input : 5 (რადიუსი )
+        // Output : 50
+        
+        Console.Write("Enter rafius of circle: ");
+        var radius = Convert.ToInt32(Console.ReadLine());
+        var bigSquare = (2* radius)  * (2* radius) ;
+        var smallSquare = bigSquare/2;
+        Console.WriteLine($"Area of big square is : {bigSquare} and small square: {smallSquare}");
+
     }
 }
